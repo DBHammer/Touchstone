9295d5b33f80d69d9d455d8566f2784bb48bce32
ylht <wqs.97@icloud.com>
2021-11-16 10:08:34 +0800

    generate conf

--- running examples/query-init/make.sh
+++ running examples/query-init/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..16}
 do
-	java SQLProduction ../data/result qtemplates/$i > query/$i.sql
+	java SQLProduction.java ../data/result qtemplates/$i > ../data/query/$i.sql
 done
